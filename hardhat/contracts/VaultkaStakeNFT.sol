78ad5ee17a53c71de193f934783023d841aa083a
Zhanglizi <1931056261@qq.com>
2024-11-02 17:41:03 +0800

    NFT合约、质押代理工厂合约初步完成，质押代理合约在推进

---
+++ hardhat/contracts/VaultkaStakeNFT.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract VaultkaStakeNFT is ERC721, ERC721Pausable, Ownable, ERC721Burnable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner) ERC721("VaultkaStakeNFT", "vsNFT") {
+        transferOwnership(initialOwner);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public onlyOwner returns (uint256){
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address to, uint256 tokenId, address auth)
+        internal
+        override(ERC721, ERC721Pausable)
+        returns (address)
+    {
+        return super._update(to, tokenId, auth);
+    }
+}
